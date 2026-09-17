ef24b6b4c90b90c6670991e61fda893d2bf7d025
byungkyuJeong <baenibkjeong@gmail.com>
2026-09-17 23:29:08 +0900

    Add files via upload

--- works.js
+++ works.js
@@ -161,8 +161,7 @@ const works = [
   {
     title: "mini_07",
     category: "미니어처",
-    image: "mini_07.jpg",
-    onImage: "mini_07_on.jpg",
+    image: "no13_mini7.jpg",
     tags: "미니어처 · 작은 세계",
     year: "2026",
     material: "포맥스, 스위치, 미니어처 조명, 종이, 나무 등",
